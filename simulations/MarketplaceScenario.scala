81ce5c959467171bae7678f05992097e3ebf89f1
john t skarbek <g@g.com>
2014-02-27 10:35:01 -0500

    Initial import of our first marketplace scenario.  Thus far it's only task is to create a service item

---
+++ simulations/MarketplaceScenario.scala
@@ -0,0 +1,32 @@
+
+import io.gatling.core.Predef._
+import io.gatling.core.session.Expression
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+import io.gatling.http.Headers.Names._
+import io.gatling.http.Headers.Values._
+import scala.concurrent.duration._
+import bootstrap._
+import assertions._
+
+class MarketplaceScenario extends Simulation {
+
+	val httpProtocol = http
+		.baseURL("http://10.40.1.115:8080")
+		.acceptHeader("application/json")
+		.acceptEncodingHeader("gzip,deflate,sdch")
+		.acceptLanguageHeader("en-US,en;q=0.8")
+		.authorizationHeader("Basic dGVzdEFkbWluMTpwYXNzd29yZA==")
+
+	val create_item_headers = Map(
+		"""Content-Type""" -> """application/json""")
+
+	val scn = scenario("Marketplace Scenario")
+		.exec(http("create service item")
+			.post("""/marketplace/api/serviceItem""")
+			.headers(create_item_headers)
+			.body(RawFileBody("create_service_item.json"))
+			.basicAuth("""testAdmin1""","""password"""))
+
+	setUp(scn.inject(atOnce(1 user))).protocols(httpProtocol)
+}
