de3ab722b650da3caf977ec02396cc52c9076912
Rob Szewczyk <rob.szew@gmail.com>
2014-03-12 00:53:32 -0400

    cleaning up feeders

--- src/test/scala/org/ozoneplatform/gatling/feeder/Feeders.scala
+++ src/test/scala/org/ozoneplatform/gatling/feeder/Feeders.scala
@@ -15,21 +15,31 @@ object Feeders {
 
   private val RNG = new Random
 
+  /**
+   * return a psuedo random integer between a and b inclusive
+   *
+   */
   def randInt(a: Int, b: Int) = RNG.nextInt((b + 1) - a) + a
 
+  /**
+   * Given an array of string, choose one at random
+   */
   def randWord(words: Array[String]): String = words(randInt(0, words.size - 1))
 
-  def randWords(words: Array[String], count: Int, acc: String = ""): String =
+  /**
+   * Given an array of strings, return an array of size count of strings chosen at random from the original list
+   */
+  def randWordList(words: Array[String], count: Int, acc: String = ""): String =
     if (count < 0) acc
-    else if (acc.size > 0) randWords(words, count - 1, randWord(words) + " " + acc)
-    else randWords(words, count - 1, randWord(words))
+    else if (acc.size > 0) randWordList(words, count - 1, randWord(words) + " " + acc)
+    else randWordList(words, count - 1, randWord(words))
 
-  def randWordList(words: Array[String], count: Int, acc: List[String] = List[String]()): List[String] = {
+  def randWordSet(words: Array[String], count: Int, acc: List[String] = List[String]()): List[String] = {
     if (acc.size == count) acc
     else {
       val word = randWord(words)
-      if (acc.contains(word)) randWordList(words, count, acc)
-      else randWordList(words, count, acc ++ List[String](word))
+      if (acc.contains(word)) randWordSet(words, count, acc)
+      else randWordSet(words, count, acc ++ List[String](word))
     }
   }
 
@@ -42,7 +52,7 @@ object Feeders {
     new Feeder[String] {
       override def hasNext = true
 
-      override def next(): Map[String, String] = Map("queryString" -> randWords(wordDistro, 5))
+      override def next(): Map[String, String] = Map("queryString" -> randWordList(wordDistro, 5))
     }
   }
 
@@ -50,7 +60,7 @@ object Feeders {
     new Feeder[String] {
       override def hasNext = true
 
-      override def next(): Map[String, String] = Map("itemTitle" -> randWords(wordDistro, randInt(1, 6)))
+      override def next(): Map[String, String] = Map("itemTitle" -> randWordList(wordDistro, randInt(1, 6)))
     }
   }
   
@@ -60,7 +70,7 @@ object Feeders {
    * @param tagCount the number of tags to put in the candidate list
    * @return
    */
-  def itemTag(tagCount: Int): Feeder[String] = wordFeed(randWordList(wordDistro, tagCount) toArray, "itemTag")
+  def itemTag(tagCount: Int): Feeder[String] = wordFeed(randWordSet(wordDistro, tagCount) toArray, "itemTag")
 
   /**
    * Chooses a random word from a list
@@ -101,12 +111,26 @@ object Feeders {
     }
   }
 
+  /**
+   * Given an array of profiles as JsObjects, return the subset of profiles from that array that are admins
+   *
+   * @param words
+   * @param acc
+   * @return
+   */
   def filterAdminUsers(words: Array[JsObject], acc: Array[JsObject] = Array[JsObject]()): Array[JsObject] = {
     if (words.size == 0) acc
     else if ((words.head \ "username").toString().contains("Admin")) filterAdminUsers(words.tail, acc ++ Array[JsObject](words.head))
     else filterAdminUsers(words.tail, acc)
   }
 
+  /**
+   * Given a string of profiles as json, filter out the admins and return a randomly chosen admin user name
+   *
+   * @param storeUsersAsJson
+   * @param propertyName
+   * @return
+   */
   def randomAdminUserName(storeUsersAsJson: String, propertyName: String = "adminUserName"): Feeder[String] = {
     new Feeder[String] {
       val storeUsers = filterAdminUsers((Json.parse(storeUsersAsJson) \ "data").as[Array[JsObject]])
@@ -116,7 +140,14 @@ object Feeders {
       override def next(): Map[String, String] = Map(propertyName -> (randomItemAsJson(storeUsers) \ "username").as[String])
     }
   }
-  
+
+  /**
+   * Given a string of profiles as json, return a randomly chosen username from that list
+   *
+   * @param storeUsersAsJson
+   * @param propertyName
+   * @return
+   */
   def randomUserName(storeUsersAsJson: String, propertyName: String = "userName"): Feeder[String] = {
     new Feeder[String] {
       val storeUsers = (Json.parse(storeUsersAsJson) \ "data").as[Array[JsObject]]
@@ -127,6 +158,12 @@ object Feeders {
     }
   }
 
+  /**
+   * Given a string of service items as json, return a randomly chosen service item id from that list
+   *
+   * @param storeItemsAsJson
+   * @return
+   */
   def randomServiceItemId(storeItemsAsJson: String): Feeder[Int] = {
     new Feeder[Int] {
       val storeItems = (Json.parse(storeItemsAsJson) \ "data").as[Array[JsObject]]
@@ -142,7 +179,7 @@ object Feeders {
   def generateUserName(isAdmin: Boolean = false, propertyName: String = "userName"): Feeder[String] = {
     new Feeder[String] {
 
-      def baseString(): String = randWords(dictionary, 4).replaceAll(" ", "")
+      def baseString(): String = randWordList(dictionary, 4).replaceAll(" ", "")
 
       override def hasNext = true
 
