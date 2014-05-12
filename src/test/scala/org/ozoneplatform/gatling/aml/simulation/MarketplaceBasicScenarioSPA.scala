6222ab5a802bf09d6e9d7f95ccb2a29bfcf45e8c
Rob Szewczyk <rob.szew@gmail.com>
2014-05-12 15:14:54 -0400

    Adding in additional search requests to simulate a filter action

--- src/test/scala/org/ozoneplatform/gatling/aml/simulation/MarketplaceBasicScenarioSPA.scala
+++ src/test/scala/org/ozoneplatform/gatling/aml/simulation/MarketplaceBasicScenarioSPA.scala
@@ -7,27 +7,25 @@ import org.ozoneplatform.gatling.aml.feeder.FeederUtils._
 import scala.concurrent.duration._
 import org.ozoneplatform.gatling.aml.builder.SearchBuilder
 import org.ozoneplatform.gatling.aml.action.ActionHelpers._
-import io.gatling.core.action.builder.ActionBuilder
 
-class MarketplaceBasicScenario extends Simulation {
+class MarketplaceBasicScenarioSPA extends Simulation {
   val userCount = getUserCount
   val userLoops = getScenarioUserCount
   val rampPeriod = getRampPeriod
 
   val tagList = FeederHelpers.randWordSet(count = 500)
 
-  def allListings: ActionBuilder = new SearchBuilder().allListings().maxResults(24).search
-  def newArrivals: ActionBuilder = new SearchBuilder().newArrivals().maxResults(24).search
-  def highestRate: ActionBuilder = new SearchBuilder().highestRated().maxResults(24).search
-  def termSearch: ActionBuilder = new SearchBuilder().searchTerm("${queryString}").maxResults(24).search
-
   val browseForListings = randomSwitch(
-    33.3 -> exec(allListings).exec(allListings),
-    33.3 -> exec(newArrivals).exec(newArrivals),
-    33.3 -> exec(highestRate).exec(highestRate)
+    33.3 -> exec(new SearchBuilder().allListings().maxResults(24).search),
+    33.3 -> exec(new SearchBuilder().newArrivals().maxResults(24).search),
+    33.3 -> exec(new SearchBuilder().highestRated().maxResults(24).search)
   )
 
-  val searchForListings = exec(termSearch).exec(termSearch) //there are two search requests issued per rendering of the search page
+  val searchForListings = feed(Feeders.wordListFeeder(propertyName = "queryString"))
+    .exec(new SearchBuilder()
+    .searchTerm("${queryString}")
+    .maxResults(24)
+    .search)
 
   val reviewChain = feed(Feeders.blurbFeeder(propertyName = "itemComment"))
     .feed(Feeders.itemRatingFeeder())
@@ -41,18 +39,14 @@ class MarketplaceBasicScenario extends Simulation {
   val basicUserScenario = scenario("Basic Marketplace Performance Scenario")
     .feed(Feeders.randomUserFeeder(userCount))
     .repeat(10) {
-      exec(getConfig)
-      .feed(Feeders.wordListFeeder(propertyName = "queryString"))
+    exec(getConfig)
       .pause(3 seconds, 12 seconds) //pause to choose search method/query
       .randomSwitch(52.0 -> browseForListings, 48.0 -> searchForListings)
       .repeat(5) {
-        pause(3 seconds, 12 seconds) //pause to scan and choose search result
-        .randomSwitch(47.0 -> exec(termSearch)) //use a filter
-        .randomSwitch(33.0 -> exec(termSearch)) //use a second filter
-        .randomSwitch(33.0 -> exec(termSearch)) //use a third filter
+      pause(3 seconds, 12 seconds) //pause to scan and choose search result
         .exec(getSearchItemAndDoChain(randomSwitch(0.25 -> reviewChain, 0.5 -> tagChain, 0.5 -> exec(addToOwf))))
-      }
     }
+  }
 
   setUp(
     basicUserScenario.inject(rampUsers(userLoops).over(rampPeriod))
